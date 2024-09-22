87c6c82d48380c14ae48c397e2efdd968906c03d
Auke de Jong <aukedejong@gmail.com>
2024-09-22 19:00:57 +0200

    Refactoring: new using DegreesCalculator to determine draw orientations, prep for next feature.

---
+++ src/renderer/DegreesCalculator.ts
@@ -0,0 +1,53 @@
+import {Log} from "../util/Log";
+
+export class DegreesCalculator {
+
+    private compassDegrees = 0;
+    private windDirectionDegrees = 0;
+    private roseRenderDegrees = 0;
+    private windDirectionRenderDegrees = 0;
+
+    constructor(private readonly northOfffset: number,
+                private readonly autoRotateByEntity: boolean) {
+        this.updateRenderDegrees();
+    }
+
+    private updateRenderDegrees() {
+        if (this.autoRotateByEntity) {
+
+            this.roseRenderDegrees = +this.northOfffset + this.compassDegrees;
+            this.windDirectionRenderDegrees = -90 + this.northOfffset + this.compassDegrees + this.windDirectionDegrees;
+
+        } else {
+
+            this.roseRenderDegrees = this.northOfffset;
+            this.windDirectionRenderDegrees = -90 + this.northOfffset + this.windDirectionDegrees;
+
+        }
+        Log.debug("Degrees sensor: ", this.windDirectionDegrees, this.compassDegrees)
+        Log.debug("Degrees for rendering: ", this.roseRenderDegrees, this.windDirectionRenderDegrees);
+    }
+
+    getWindDirectionRenderDegrees(): number {
+        return this.windDirectionRenderDegrees;
+    }
+
+    getRoseRenderDegrees(): number {
+        return this.roseRenderDegrees;
+    }
+
+    setWindDirectionDegrees(degrees: number) {
+        this.windDirectionDegrees = degrees;
+        this.updateRenderDegrees();
+    }
+
+    setCompassDegrees(degrees: number) {
+        this.compassDegrees = degrees;
+        this.updateRenderDegrees();
+    }
+
+
+
+
+
+}
