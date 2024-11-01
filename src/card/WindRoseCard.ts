0b4086c17a13bbb8654b8bc7837bc4806e06690d
Auke de Jong <aukedejong@gmail.com>
2024-11-01 16:08:10 +0100

    Version to 1.11.6

--- src/card/WindRoseCard.ts
+++ src/card/WindRoseCard.ts
@@ -20,7 +20,7 @@ import {Log2} from "../util/Log2";
 
 /* eslint no-console: 0 */
 console.info(
-    `%c  WINROSE-CARD  %c Version 1.11.5 `,
+    `%c  WINROSE-CARD  %c Version 1.11.6 `,
     'color: orange; font-weight: bold; background: black',
     'color: white; font-weight: bold; background: dimgray',
 );
